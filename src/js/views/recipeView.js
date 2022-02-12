fd8963262ebd407bb95c30d583730f22075709eb
Lari Oncescu <larioncescu@yahoo.com>
2022-02-12 17:18:41 +0200

    refactor code

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -90,26 +90,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
-            .map(ingredient => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${
-                ingredient.quantity
-                  ? new Fraction(ingredient.quantity).toString()
-                  : ''
-              }</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ingredient.unit}</span>
-                ${ingredient.description}
-              </div>
-            </li>
-            `;
-            })
-            .join('')}
+          ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
       </div>
 
       <div class="recipe__directions">
@@ -133,6 +114,23 @@ class RecipeView {
         </a>
       </div> 
     `;
+  }
+
+  #generateMarkupIngredient(ingredient) {
+    return `
+  <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${icons}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+    }</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${ingredient.unit}</span>
+      ${ingredient.description}
+    </div>
+  </li>
+  `;
   }
 }
 
