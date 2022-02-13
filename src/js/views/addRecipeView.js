5c465ba2f5096d9600f3eb5b310d06226de6d1d4
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 19:46:10 +0200

    create handler show window

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -6,6 +6,15 @@ class AddRecipeView extends View {
 
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', function () {
+      this._overlay.classList.toggle('hidden');
+      this._window.classList.toggle('hidden');
+    });
+  }
 
   _generateMarkup() {}
 }
