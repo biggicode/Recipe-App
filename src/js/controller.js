bd22f424e0781245744fd0a7f077f8d1c2f0738c
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 17:39:47 +0200

    add delete bookmark

--- src/js/controller.js
+++ src/js/controller.js
@@ -71,7 +71,7 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
-  model.addBookmark(model.state.recipe);
+  if (!model.state.recipe) model.addBookmark(model.state.recipe);
   console.log(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -80,3 +80,12 @@ export const addBookmark = function (recipe) {
   //Mark current recipe
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
 };
+
+export const deleteBookmark = function (id) {
+  //delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  //Mark current recipe not bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = false;
+};
