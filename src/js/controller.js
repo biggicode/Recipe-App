d6bd020390f6273ccb19663c897c80241b8a3859
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 23:40:56 +0200

    take data from form and change it

--- src/js/controller.js
+++ src/js/controller.js
@@ -91,7 +91,7 @@ const controlBookmarks = function () {
 };
 
 const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+  model.uploadRecipe(newRecipe);
 };
 
 const init = function () {
