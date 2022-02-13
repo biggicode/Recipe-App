9658f3568e0a6ac7439ffc1d0d3c3b1221211201
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 15:26:18 +0200

    compare nodes

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -20,7 +20,22 @@ export default class View {
     this._data = data;
     const newMarkup = this._generateMarkup();
 
+    //create virtual dom
     const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    //get elements from page
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+    });
   }
 
   _clear() {
