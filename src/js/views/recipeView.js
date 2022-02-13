404b7ab8690d657903d84c19c6061cbfef99e3c1
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 13:46:48 +0200

    add guard clause

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -15,6 +15,7 @@ class RecipeView extends View {
   addHandlerUpdateServings(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--tiny');
+      if (!btn) return;
     });
   }
 
