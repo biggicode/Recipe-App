bdc9e1e9f5ce036425f08c64e28d777a6b2a85ac
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 22:13:46 +0200

    create handler for form

--- src/js/controller.js
+++ src/js/controller.js
@@ -90,6 +90,10 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -97,5 +101,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
