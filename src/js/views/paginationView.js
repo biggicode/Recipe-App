7631c1e0035cf502d70a9fc62f1499fe29505b17
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 11:10:20 +0200

    add pagination view

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,8 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {}
+}
