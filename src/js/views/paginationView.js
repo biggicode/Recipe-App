e086db8f0403091d6a545c329c8126870ac59c67
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 11:30:53 +0200

    add basic logic for pagination

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,10 +5,28 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const numOfPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numOfPages);
+
     //First page and there are more pages to display
-    //First page and there are no more pages to display
+    if (this._data.page === 1 && numOfPages > 1) {
+      return 'page1 and others';
+    }
+
     //Last page
+    if (this._data.page === numOfPages && numOfPages > 1) {
+      return 'last page';
+    }
+
     //Other page
+    if (this._data.page < numOfPages) {
+      return 'other page';
+    }
+
+    //First page and there are no more pages to display
+    return 'only one page';
   }
 }
 
