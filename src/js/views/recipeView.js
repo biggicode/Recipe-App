29db011ec322fd47b080a99eca91a8c60c81101b
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 17:23:22 +0200

    add guard clause and method to recipe view

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -23,6 +23,14 @@ class RecipeView extends View {
     });
   }
 
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+
+      if (!btn) return;
+    });
+  }
+
   _generateMarkup() {
     //Render recipe
     return `
@@ -74,9 +82,9 @@ class RecipeView extends View {
 
         <div class="recipe__user-generated">
         </div>
-        <button class="btn--round">
+        <button class="btn--round btn--bookmark">
           <svg class="">
-            <use href="${icons}#icon-bookmark-fill"></use>
+            <use href="${icons}#icon-bookmark"></use>
           </svg>
         </button>
       </div>
