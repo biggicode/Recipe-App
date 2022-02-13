ac4c3e2579ae79372ce183ed40bac120ac7e1f21
Lari Oncescu <larioncescu@yahoo.com>
2022-02-13 23:48:26 +0200

    handle error

--- src/js/controller.js
+++ src/js/controller.js
@@ -90,8 +90,13 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  model.uploadRecipe(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    await model.uploadRecipe(newRecipe);
+  } catch (error) {
+    console.log(error);
+    addRecipeView.renderError(error.message);
+  }
 };
 
 const init = function () {
