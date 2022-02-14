7a3c376601852eca6815650f478985bab316e933
Lari Oncescu <larioncescu@yahoo.com>
2022-02-14 19:52:13 +0200

    solve bug

--- src/js/model.js
+++ src/js/model.js
@@ -119,7 +119,8 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
-        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        const ingArr = ing[1].split(',').map(el => el.trim());
+        // const ingArr = ing[1].replaceAll(' ', '').split(',');
         if (ingArr.length !== 3) throw new Error('Wrong ingredient format');
 
         const [quantity, unit, description] = ingArr;
